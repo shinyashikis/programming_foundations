50c8efb975684085e5162dbedb5e8c1424afc7f3
shinyashikis <shinyashikis@gmail.com>
2015-10-07 06:44:02 +0900

    2.4

---
+++ ch2/2.4.ml
@@ -0,0 +1,17 @@
+true ;;
+false ;;
+true && true ;;
+true && false ;;
+true || false ;;
+false || false ;;
+not true ;;
+not false ;;
+not (false || not false && not false) ;;
+not (false || ((not false) && (not false))) ;;
+2 < 3 ;;
+2 < 3 && 2.0 >= 3.0 ;;
+2 < 3 || 2.0 = 3.0 ;;
+not (3 < 2) ;;
+true > false ;;
+false > true ;;
+true > true ;;
