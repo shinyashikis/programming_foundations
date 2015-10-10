1265ebca23b445629ad8c7e99f2556e202cdabde
shinyashikis <shinyashikis@gmail.com>
2015-10-10 09:09:35 +0900

    5.2

---
+++ ch5/5.2.ml
@@ -0,0 +1,4 @@
+if true then 1 else 2 ;;
+if false then 1 else 2 ;;
+if 1 < 2 then 3 else 4 ;;
+if 3 + 4 < 5 then 1 else 2 ;;
---
+++ ch5/q5.1.ml
@@ -0,0 +1,14 @@
+(* 1 *)
+if 2 < 1 then 3 else 4 ;;
+
+(* 2 *)
+if "true" then 3.14 else 2.72 ;;
+
+(* 3 *)
+if "a" = "b" then false else true ;;
+
+(* 4 *)
+if true < false then 1 else "2" ;;
+
+(* 5 *)
+if not (3 = 4) then 1 < 2 else 1 > 2 ;;
