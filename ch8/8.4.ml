6e856dac84327e53b69ba88a00d3880c1fee5f49
shinyashikis <shinyashikis@gmail.com>
2015-10-13 23:15:44 +0900

    8.4

---
+++ ch8/8.4.ml
@@ -0,0 +1,10 @@
+type gakusei_t = {
+  namae : string;
+  tensuu : int;
+  seiseki : string;
+} ;;
+
+let asai = {namae = "asai"; tensuu = 70; seiseki = "B"} ;;
+asai.namae ;;
+asai.tensuu ;;
+asai.seiseki ;;
