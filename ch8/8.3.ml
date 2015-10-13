2610811c8e1e804576c36feb9dce6a8553592bdd
shinyashikis <shinyashikis@gmail.com>
2015-10-13 23:13:00 +0900

    8.3

--- ch8/8.3.ml
+++ ch8/8.3.ml
@@ -9,3 +9,14 @@ let tuuchi gakusei = match gakusei with
     n ^ "さんは" ^ string_of_int t ^ "点で、成績は" ^ s ^ "です" ;;
 
 tuuchi {namae = "asai"; tensuu = 70; seiseki = "B"} ;;
+
+let hyouka gakusei = match gakusei with
+  {namae = n; tensuu = t; seiseki = s} ->
+    {namae = n;
+     tensuu = t;
+     seiseki = if t >= 80 then "A"
+               else if t >= 70 then "B"
+               else if t >= 60 then "C"
+               else "D"} ;;
+
+hyouka {namae = "asai"; tensuu = 70; seiseki = ""} ;;
