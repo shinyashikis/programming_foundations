2e4ecc1ec705f5dd4915a890e94183d865db8411
shinyashikis <shinyashikis@gmail.com>
2015-11-09 22:33:48 +0900

    fix 12.4

--- ch12/q12.3.ml
+++ ch12/q12.3.ml
@@ -1,7 +1,5 @@
 # use "q12.2.ml"
 
-let ekimei_list = make_eki_list global_ekimei_list 
-
 (* 目的：eki_t 型のリストと起点（駅名漢字）を受け取ったら、起点のみ初期化したリストを返す *)
 (* shokika : eki_t list -> string -> eki_t list *)
 let rec shokika lst kanji = match lst with
