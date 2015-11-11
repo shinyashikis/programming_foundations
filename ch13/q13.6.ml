a7245affde567b541ac029b5ec0b58be72f2d176
shinyashikis <shinyashikis@gmail.com>
2015-11-11 22:22:18 +0900

    13.5

--- ch13/q13.6.ml
+++ ch13/q13.6.ml
@@ -4,21 +4,22 @@
 # use "../ch12/q12.2.ml"
 # use "../ch12/q12.3.ml"
 
-(* eki_t list *)
-let ekimei_list = make_eki_list global_ekimei_list 
-(* eki_t list *)
-let shokika_list = shokika ekimei_list "四ツ谷"
+let yotsuya = {namae = "四ツ谷"; saitan_kyori = 0.; temae_list = ["四ツ谷"]}
+let yotsuya_sanchome = {namae = "四谷三丁目"; saitan_kyori = infinity; temae_list = []}
+let shinjyuku_gyoen = {namae = "新宿御苑前"; saitan_kyori = infinity; temae_list = []}
 
 let rec saitan p q ekikan_list = match ekikan_list with
-    [] -> 0.
+    [] -> infinity
   | first :: rest ->
-    if first.kiten = p && first.shuten = q then first.kyori
+    if first.kiten = p.namae && first.shuten = q.namae then first.kyori
     else saitan p q rest
- 
+
+(* 直前に確定した駅 p と未確定の駅 q を受け取ったら必要に応じて最短距離と手順リストを更新した q を返す *) 
 let koushin1 p q =
   let kyori = saitan p q global_ekikan_list
-  in if kyori > 0. then {namae = q; saitan_kyori = kyori; temae_list = [q;p]}
-  else {namae = q; saitan_kyori = infinity; temae_list = []}
+    in if q.saitan_kyori <= kyori then q 
+       else {namae = q.namae; saitan_kyori = kyori; temae_list = [q.namae;p.namae]}
 
-let test1 = koushin1 "四ツ谷" "四谷三丁目" = {namae = "四谷三丁目"; saitan_kyori = 1.0; temae_list = ["四谷三丁目";"四ツ谷"]}
-let test2 = koushin1 "四ツ谷" "新宿御苑前" = {namae = "新宿御苑前"; saitan_kyori = infinity; temae_list = []}
+(* テスト *)
+let test1 = koushin1 yotsuya yotsuya_sanchome = {namae = "四谷三丁目"; saitan_kyori = 1.0; temae_list = ["四谷三丁目";"四ツ谷"]}
+let test2 = koushin1 yotsuya shinjyuku_gyoen = shinjyuku_gyoen
